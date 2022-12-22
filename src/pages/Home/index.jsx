1bc70e55ea0b2afcab837d0b2276a8cc274a3321
Gabriel <gabrielhalvespg@gmail.com>
2022-12-22 20:53:32 -0300

    home page under development

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,6 @@
-import { Container } from './styles';
+import { Container, Banner, Main } from './styles';
+
+import bannerImg from '../../assets/bannerHome.svg';
 
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
@@ -7,6 +9,21 @@ export function Home(){
   return (
     <Container>
       <Header/>
+      
+      <Banner>
+        <img src={bannerImg} alt="Imagem do banner" />
+
+        <div>
+          <h1>Sabores inigualáveis</h1>
+          <p>Sinta o cuidado do preparo com ingredientes selecionados</p>
+        </div>
+      </Banner>
+
+      <Main>
+        <h1>Pratos principais</h1>
+
+      </Main>
+
       <Footer/>
     </Container>
   )
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,57 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+`;
 
+export const Banner = styled.div`
+  width: 112.0rem;
+  height: 26.0rem;
+
+  margin: 0 auto;
+
+  margin-top: 16.4rem;
+
+  background: linear-gradient(180deg, #091E26 0%, #00131C 100%);
+  border-radius: 8px;
+
+  display: flex;
+
+  align-items: center;
+
+  img {
+    margin-bottom: 15.0rem;
+    position: relative;
+    left: -50px;
+  }
+
+  h1 {
+    font-weight: 500;
+    font-size: 4.0rem;
+  }
+
+  p {
+    font-weight: 400;
+    font-size: 1.5rem;
+    margin-top: 0.8rem;
+  }
+
+  h1, p {
+    font-family: var(--ff-secondary);
+  }
+`;
+
+export const Main = styled.div`
+  width: 112.0rem;
+
+  margin: 0 auto;
+
+  margin-top: 6.2rem;
+
+  h1 {
+    font-family: var(--ff-secondary);
+    font-weight: 500;
+    font-size: 3.2rem;
+
+    margin-bottom: 3.9rem;
+  }
 `;
